9f9fa1c24b7ec4822873770aec306240bb8aaf58
flavio-bernasconi <info@flaviobernasconi.it>
2019-07-18 14:58:13 +0200

    .

--- main.js
+++ main.js
@@ -1,9 +1,53 @@
 $( document ).ready(function() {
+
+
+  //funziona con hover
+    // $(".drop").mouseenter(
+    //   function(){
+    //     $(this).children(".content-drop").addClass("active");
+    //   }
+    // );
+    // $(".drop").mouseleave(
+    //   function(){
+    //     $(this).children(".content-drop").removeClass("active");
+    //   }
+    // );
+
+
+    var cliccato = false;
+
     $(".drop").click(
       function(){
         $(this).children(".content-drop").toggleClass("active");
+        cliccato = true;
+        console.log(cliccato);
       }
-    )
+    );
+
+    $(".drop2").click(
+      function(){
+        if (cliccato == true) {
+          $(this).children(".content-drop").addClass("active");
+          $(".drop").children(".content-drop").removeClass("active");
+          cliccato = false;
+          console.log(cliccato);
+        }
+        else{
+          $(this).children(".content-drop").removeClass("active");
+
+        }
+      }
+    );
+
+
+
+
+
+
+
+
+
+
 });
 
 //
