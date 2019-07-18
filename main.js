7d9eeabd6af868760ac8f0ff07cd1fee6a1acb08
flavio-bernasconi <info@flaviobernasconi.it>
2019-07-18 18:34:52 +0200

    funziona il js. lavoro sul css

--- main.js
+++ main.js
@@ -1,44 +1,36 @@
 $( document ).ready(function() {
 
 
-  //funziona con hover
-    // $(".drop").mouseenter(
-    //   function(){
-    //     $(this).children(".content-drop").addClass("active");
-    //   }
-    // );
-    // $(".drop").mouseleave(
-    //   function(){
-    //     $(this).children(".content-drop").removeClass("active");
-    //   }
-    // );
-
-
-    var cliccato = false;
 
     $(".drop").click(
       function(){
-        $(this).children(".content-drop").toggleClass("active");
-        cliccato = true;
-        console.log(cliccato);
-        if (cliccato == true) {
-          $(".drop2").children(".content-drop").removeClass("active");
-        }
-      }
-
-    );
-
-    $(".drop2").click(
-      function(){
-        if (cliccato == true) {
-          $(this).children(".content-drop").addClass("active");
-          $(".drop").children(".content-drop").removeClass("active");
-          cliccato = false;
-          console.log(cliccato);
-        }
-
+        //rimuovi a tutti la classe active
+        $(".content-drop").removeClass("active");
+        //aggiungi la classe attiva solo al figlio di questo drop
+        $(this).children(".content-drop").addClass("active");
       }
     );
+
+    $(document).click(function(){
+      $(".content-drop").removeClass("active");
+    });
+
+    $(".drop").click(function(e){
+      e.stopPropagation();
+    });
+
+
+    //funziona con hover
+      // $(".drop").mouseenter(
+      //   function(){
+      //     $(this).children(".content-drop").addClass("active");
+      //   }
+      // );
+      // $(".drop").mouseleave(
+      //   function(){
+      //     $(this).children(".content-drop").removeClass("active");
+      //   }
+      // );
 
 
 
