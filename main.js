ee179039dd8b05f54bc3c6792b1fb939df365f75
flavio-bernasconi <info@flaviobernasconi.it>
2019-07-19 18:05:54 +0200

    .

--- main.js
+++ main.js
@@ -5,12 +5,11 @@ $( document ).ready(function() {
     $(".drop").click(
       function(){
         //rimuovi a tutti la classe active
-        $(".content-drop").removeClass("active");
+        $(".drop").not(this).find(".content-drop").removeClass("active");
         //aggiungi la classe attiva solo al figlio di questo drop
-        $(this).children(".content-drop").addClass("active");
+        $(this).children(".content-drop").toggleClass("active");
       }
     );
-
 
 
     //se clicchi da qualche parte chiude il dropdown
